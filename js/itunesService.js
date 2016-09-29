461be9d2e2189ca18da17303f1ca0d8c6702d318
Henry Reed <henryreed@Henrys-MacBook-Pro.local>
2016-09-29 16:39:07 -0600

    itunes clone  project

--- js/itunesService.js
+++ js/itunesService.js
@@ -4,9 +4,31 @@ angular.module('itunes').service('itunesService', function($http, $q){
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+    this.getArtist = function(artist) {
+      var deferred = $q.defer();
+       $http({
+        method: 'JSONP',
+        url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+      }).then(function(response) {
+        var parsedResponse = response.data.results;
+        var blkArr = [];
+        for(var i = 0; i < parsedResponse.length; i++) {
+          blkArr.push({
+            Play: parsedResponse[i].previewUrl,
+            Artist: parsedResponse[i].artistName,
+            Collection: parsedResponse[i].collectionName,
+            AlbumArt: parsedResponse[i].artworkUrl100,
+            Type: "song",
+            CollectionPrice: parsedResponse[i].collectionPrice
+          })
+        }
+      deferred.resolve(blkArr)
+    })
+    return deferred.promise;
+  }
+
 });
